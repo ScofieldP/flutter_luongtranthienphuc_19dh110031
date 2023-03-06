749c7fb447035bfb6e91ce5617fdebee5bfef3f9
ScofieldP <thienphuc.luongtran@gmail.com>
2023-03-06 22:40:21 +0700

    setup models

---
+++ lib/models/products.dart
@@ -0,0 +1,9 @@
+class Products {
+  int id;
+  String title;
+  String description;
+  String image;
+  double price;
+  Products({required this.id, required this.title, required this.description, required this.image, required this.price});
+}
+
